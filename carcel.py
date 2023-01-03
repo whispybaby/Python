242fcbcb92abc8b4ef33d908ef229f177823eef6
Charlotte Rodriguez <euphoriahead@gmail.com>
2023-01-03 08:30:16 -0300

    commit de parametros

---
+++ carcel.py
@@ -0,0 +1,9 @@
+#Actividad te  dieron 100 años de condena en la carcel 
+# porque no te gusta la pizza con fruta
+# usa la funcion rango para mostrarla por decadas 
+for años in range(0,101,10):
+    #El primer parametro indica que queremos comenzar desde 0
+    #El segundo parametro considera el numero de elementos y si le ponemos 100  obtendreiamos el rango
+    # del 0 al 90
+    #Para el tercer y ultimo parametro va asi porqu es como se transforma en decadas
+    print(años, end=" ")
