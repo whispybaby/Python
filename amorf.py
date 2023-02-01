85eab235d03dd6bc01d73152017a8b2ba5c93055
Charlotte Rodriguez <euphoria.head@gmail.com>
2023-01-31 21:26:40 -0300

    Kitobal te adoro

--- amorf.py
+++ amorf.py
@@ -2,7 +2,7 @@ def amor():
     cantidad_amor = input("cuanto me amas: ")
     try:
         cantidad_amor = int(cantidad_amor)
-        print(f"Te amo {cantidad_amor*2}")
+        print(f"Te amo {cantidad_amor*1000} veces mas")
     except:
         print("Eso no es un numero")
 amor()
---
+++ renombre.py
@@ -0,0 +1,3 @@
+mensaje = "Hola mi cristobal te queria recordar que te amo muchisimo"
+mensaje = mensaje.replace("cristobal", "pollito")
+print(mensaje)
