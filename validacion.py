0aeed272ed87a9bf4c86baa9d0b15d824d00d9b3
Charlotte Giselle Rodriguez Muñoz <euphoria.head@gmail.com>
2023-08-21 14:04:17 -0400

    validacion.py
    Mini ejercicio de repaso para validar los datos

---
+++ validacion.py
@@ -0,0 +1,8 @@
+usuarios_baniados = ['carlos', 'aylin', 'jose', 'maria']
+usuario = 'gwen'
+
+
+if usuario not in usuarios_baniados:
+    print('El usuario no esta baniado')
+else:
+    print('El usuario esta baniado')
